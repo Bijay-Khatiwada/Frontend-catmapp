d477acd6a450ec51fbcebace4c19656d698c0ae1
Bijay Khatiwada <bijaymstry@gmail.com>
2025-07-28 13:14:27 +0100

    database base url chnaged to mongo cloud services for online deployment

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,46 +1,50 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+
+const BASE_URL = 'https://catmapp-backend.onrender.com';
 
 @Injectable()
 export class WebService {
   tasks_list: any;
   sumamry_data: any;
+
   constructor(private http: HttpClient) {}
+
   getTasks() {
-        return this.http.get(
-            'http://127.0.0.1:5000/tasks'
-            ).subscribe((response: any) => {
-                this.tasks_list = response;
-                console.log("here is data",response);       
-            })
-    }
+    return this.http.get(`${BASE_URL}/tasks`).subscribe((response: any) => {
+      this.tasks_list = response;
+      console.log("here is data", response);
+    });
+  }
+
   getTasksObservable(): Observable<any> {
-    return this.http.get('http://127.0.0.1:5000/tasks');
-    }  
+    return this.http.get(`${BASE_URL}/tasks`);
+  }
+
   addTask(newTask: any): Observable<any> {
     let postData = {
-        name: newTask.name,
-        label: newTask.label,
-        time: newTask.time,
-        description: newTask.description,
-        date: newTask.date,
-        status: newTask.status,
-        priority: newTask.priority
+      name: newTask.name,
+      label: newTask.label,
+      time: newTask.time,
+      description: newTask.description,
+      date: newTask.date,
+      status: newTask.status,
+      priority: newTask.priority
     };
-    console.log("saving",{postData})
-    return this.http.post('http://127.0.0.1:5000/addTask', postData)
-    }    
+    console.log("saving", { postData });
+    return this.http.post(`${BASE_URL}/addTask`, postData);
+  }
+
   getTask(id: any): Observable<any> {
-    return this.http.get(`http://127.0.0.1:5000/task/${id}`);
+    return this.http.get(`${BASE_URL}/task/${id}`);
   }
 
   deleteTask(id: any): Observable<any> {
-    console.log('id',id)
-    return this.http.delete(`http://127.0.0.1:5000/task-del/${id}`);
+    console.log('id', id);
+    return this.http.delete(`${BASE_URL}/task-del/${id}`);
   }
-  
+
   updateTask(id: any, updated_task: any): Observable<any> {
     let putData = {
       name: updated_task.name,
@@ -50,104 +54,91 @@ export class WebService {
       date: updated_task.date,
       status: updated_task.status,
       priority: updated_task.priority
-
     };
-    console.log({putData})
-    return this.http.put(`http://127.0.0.1:5000/update-task/${id}`, putData);
+    console.log({ putData });
+    return this.http.put(`${BASE_URL}/update-task/${id}`, putData);
   }
+
   registerUser(userData: any): Observable<any> {
-    return this.http.post('http://127.0.0.1:5000/register', userData);
+    return this.http.post(`${BASE_URL}/register`, userData);
   }
-  
+
   loginUser(credentials: any): Observable<any> {
-    return this.http.post('http://localhost:5000/login', credentials);
+    return this.http.post(`${BASE_URL}/login`, credentials);
   }
-  //pie chart labels
+
   getTimeSummary(): Observable<any> {
-  return this.http.get(
-      'http://127.0.0.1:5000/task-summary/time'
-      ).pipe(
-        map((response: any) => {
-          console.log("here is summary data",response);
-          return response;
-        }),
-         map((data: any[]) => {
-          return data.map((item: any) => ({
-            _id: item._id,
-            total_time: item.total_time,
-          }));
-        })
-      );
-}
-  getDateSummary(startDate:string,endDate:string): Observable<any> {
-  return this.http.get(
-      'http://127.0.0.1:5000/task-summary/date',
-      { 
-        params: {
-          start_date: startDate,
-          end_date: startDate, endDate
-        }
+    return this.http.get(`${BASE_URL}/task-summary/time`).pipe(
+      map((response: any) => {
+        console.log("here is summary data", response);
+        return response;
+      }),
+      map((data: any[]) =>
+        data.map((item: any) => ({
+          _id: item._id,
+          total_time: item.total_time
+        }))
+      )
+    );
+  }
+
+  getDateSummary(startDate: string, endDate: string): Observable<any> {
+    return this.http.get(`${BASE_URL}/task-summary/date`, {
+      params: {
+        start_date: startDate,
+        end_date: endDate
       }
-      ).pipe(
-        map((response: any) => {
-          console.log("here is summary data",response);
-          return response;
-        }),
-         map((data: any[]) => {
-          return data.map((item: any) => ({
-            _id: item._id,
-            total_time: item.total_time,
-          }));
-        })
-      );
-}
-getStatusSummary(): Observable<any> {
-  return this.http.get(
-      'http://127.0.0.1:5000/task-summary/status'
-      ).pipe(
-        map((response: any) => {
-          console.log("here is summary data",response);
-          return response;
-        }),
-         map((data: any[]) => {
-          return data.map((item: any) => ({
-            _id: item._id,
-            total_time: item.total_time,
-          }));
-        })
-      );
-}
-getPrioritySummary(): Observable<any> {
-  return this.http.get(
-      'http://127.0.0.1:5000/task-summary/priority'
-      ).pipe(
-        map((response: any) => {
-          console.log("here is summary data",response);
-          return response;
-        }),
-         map((data: any[]) => {
-          return data.map((item: any) => ({
-            _id: item._id,
-            total_time: item.total_time,
-          }));
-        })
-      );
-}
-getLabelMultilineSummary(): Observable<any> {
-  return this.http.get(
-      'http://127.0.0.1:5000/task-summary/multiline-plot'
-      ).pipe(
-        map((response: any) => {
-          console.log("here is multiline-plot data",{response});
-          return response;
-        }),
-         map((data: { [key: string]: { date: string, total_time: number }[] }) => {
-          return data;
-        })
-      );
-}
+    }).pipe(
+      map((response: any) => {
+        console.log("here is summary data", response);
+        return response;
+      }),
+      map((data: any[]) =>
+        data.map((item: any) => ({
+          _id: item._id,
+          total_time: item.total_time
+        }))
+      )
+    );
+  }
 
+  getStatusSummary(): Observable<any> {
+    return this.http.get(`${BASE_URL}/task-summary/status`).pipe(
+      map((response: any) => {
+        console.log("here is summary data", response);
+        return response;
+      }),
+      map((data: any[]) =>
+        data.map((item: any) => ({
+          _id: item._id,
+          total_time: item.total_time
+        }))
+      )
+    );
+  }
 
-  
+  getPrioritySummary(): Observable<any> {
+    return this.http.get(`${BASE_URL}/task-summary/priority`).pipe(
+      map((response: any) => {
+        console.log("here is summary data", response);
+        return response;
+      }),
+      map((data: any[]) =>
+        data.map((item: any) => ({
+          _id: item._id,
+          total_time: item.total_time
+        }))
+      )
+    );
+  }
+
+  getLabelMultilineSummary(): Observable<any> {
+    return this.http.get(`${BASE_URL}/task-summary/multiline-plot`).pipe(
+      map((response: any) => {
+        console.log("here is multiline-plot data", { response });
+        return response;
+      }),
+      map((data: { [key: string]: { date: string, total_time: number }[] }) => data)
+    );
+  }
 }
-
